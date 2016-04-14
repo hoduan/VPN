493645601d41aeefbb92e1b331b6359f4f3f8b06
Seed <seed@ubuntu.(none)>
2016-04-13 18:50:26 -0700

    first version of server.c

---
+++ routingG2.sh
@@ -0,0 +1,8 @@
+sudo ip addr add 10.0.3.13/24 dev toto0
+sudo ifconfig toto0 up
+
+sudo sysctl net.ipv4.ip_forward=1
+#sudo route add -net 10.0.4.10 netmask 255.255.255.255 dev toto0
+#sudo route add -net 192.168.98.0 netmask 255.255.255.0 dev toto0
+sudo route add -net 192.168.99.0 netmask 255.255.255.0 dev toto0
+
